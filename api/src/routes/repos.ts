a383aac9270665a4373acbb5487c8c88c18b8330
Daryl Nauman <darylnauman@gmail.com>
2021-12-05 15:15:59 -0500

    imported repo data from api file on server

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response } from 'express';
+import * as localRepos from '../../data/repos.json';
 
 export const repos = Router();
+
+// function falseFork(ele_val: object) {
+//   return ele_val.fork === true;
+// }
+
+// const filteredRepos = localRepos.filter(falseFork);
+// console.log(filteredRepos);
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(localRepos);
 });
